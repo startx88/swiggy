e5cddffd1d6c40e03c569fb738fd6c60686104db
suprabhat <suprabhat.biswal@rsystems.com>
2021-06-01 17:49:10 +0530

    update pagination

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -16,17 +16,23 @@ export class DataTableComponent implements OnInit {
   @Input() data: any[];
   @Input() hideColumns: string[] = ['createdAt', 'updatedAt', 'id'];
   @Input() isAction: boolean = true;
+  @Input() limit: number = 5;
   @Output() onActivate = new EventEmitter<string>();
   @Output() onDeactivate = new EventEmitter<string>();
   @Output() onDelete = new EventEmitter<string>();
   @Output() onEdit = new EventEmitter<{ item: any }>();
+  page: number = 1;
+  totalPage: number;
   constructor() {}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.totalPage = Math.ceil(this.data.length / this.limit);
+  }
+
   // track by
   trackById(id: string, item: any) {
     return item.id;
   }
-
   // handler
   onEditHandler(item: any) {
     this.onEdit.emit(item);
@@ -39,5 +45,17 @@ export class DataTableComponent implements OnInit {
   }
   onDeleteHandler(id: string) {
     this.onDelete.emit(id);
+  }
+
+  onPrev() {
+    if (this.page > 0) {
+      this.page--;
+    }
+  }
+  onNext() {
+    console.log(this.totalPage);
+    if (this.page < this.totalPage) {
+      this.page++;
+    }
   }
 }
