c481dd5b7104f04178b857636c061a1d007c0fb3
suprabhat <suprabhat.biswal@rsystems.com>
2021-06-03 18:33:39 +0530

    add partner registraton page

--- src/app/views/partner-with-us/partner-with-us.component.ts
+++ src/app/views/partner-with-us/partner-with-us.component.ts
@@ -8,17 +8,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PartnerWithUsComponent implements OnInit {
   @Input() bg: string;
-  formGroup: FormGroup;
+  form: FormGroup;
+  confirmPasswordError: boolean = false;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     // initialize form
-    this.formGroup = this.fb.group({
+    this.form = this.fb.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
       mobile: ['', Validators.required],
     });
+
+    this.form.valueChanges.subscribe((data) => {
+      if (data.password === data.confirmPassword) {
+        this.confirmPasswordError = true;
+      } else {
+        this.confirmPasswordError = false;
+      }
+    });
+  }
+
+  // firstname
+  get firstname() {
+    return this.form.get('firstname');
+  }
+  // lastname
+  get lastname() {
+    return this.form.get('lastname');
+  }
+  // email
+  get email() {
+    return this.form.get('email');
+  }
+  // password
+  get password() {
+    return this.form.get('password');
+  }
+  // password
+  get confirm() {
+    return this.form.get('confirmPassword');
+  }
+  // mobile
+  get mobile() {
+    return this.form.get('mobile');
+  }
+  noPartnerRegistration() {
+    if (this.form.invalid) return;
   }
 }
