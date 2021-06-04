c474ed54390e5bc069d0fe887c0fecc947f56430
suprabhat <suprabhat.biswal@rsystems.com>
2021-06-04 20:56:46 +0530

    update product item

--- src/app/shared/components/product-item/product-item.component.ts
+++ src/app/shared/components/product-item/product-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent implements OnInit {
+  @Input() product: IProduct;
+  @Input() isCartBtn: boolean = false;
+  @Input() type: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
